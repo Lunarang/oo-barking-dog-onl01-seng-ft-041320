7f46f36e2f5091b2d2271eeef63dee99e73adce4
Erin Mettille <ecmettille@gmail.com>
2020-04-21 18:08:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 # Your code goes here!
-class Dog 
-  def name=(this_dogs_name)
-    this_dogs_name = name
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
   end
   
-  def name
-    name
-    
+  def bark
+    puts "woof!"
+  end
 end
+ 
+
+ 
